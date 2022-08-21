c8d35e4b176651df9d5bf0f90dc122e488c02f6b
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-21 17:32:00 +0200

    Update product.js

--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -4,6 +4,15 @@ export default {
     return {
       products: [
         {
+          id: "p3",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/640px-Good_Food_Display_-_NCI_Visuals_Online.jpg",
+          title: "Food Box",
+          description:
+            "May be partially expired when it arrives but at least it is cheap!",
+          price: 6.99,
+        },
+        {
           id: "p1",
           image:
             "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Books_HD_%288314929977%29.jpg/640px-Books_HD_%288314929977%29.jpg",
@@ -19,15 +28,6 @@ export default {
           title: "Mountain Tent",
           description: "A tent for the ambitious outdoor tourist.",
           price: 129.99,
-        },
-        {
-          id: "p3",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/640px-Good_Food_Display_-_NCI_Visuals_Online.jpg",
-          title: "Food Box",
-          description:
-            "May be partially expired when it arrives but at least it is cheap!",
-          price: 6.99,
         },
       ],
     };
