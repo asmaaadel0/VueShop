ffccff84706d42fbd754905ba2ef0d84fe9c50f0
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-08-21 17:10:28 +0200

    Update CartItem.vue

--- src/components/cart/CartItem.vue
+++ src/components/cart/CartItem.vue
@@ -23,7 +23,6 @@
 
 <script>
 export default {
-  inject: ['removeProductFromCart'],
   props: ['prodId', 'title', 'image', 'price', 'qty'],
   computed: {
     itemTotal() {
@@ -32,7 +31,7 @@ export default {
   },
   methods: {
     remove() {
-      this.removeProductFromCart(this.prodId);
+      this.$store.dispatch('cart/removeFromCart', { productId: this.prodId });
     }
   }
 };
